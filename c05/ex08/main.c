f399ba791ac8eeef9c5e5a69d3d1868f79b0b2d0
2LeoCode <leo.13suardi@outlook.com>
2020-03-09 20:26:46 +0100

    commentaries

--- c05/ex08/main.c
+++ c05/ex08/main.c
@@ -4,8 +4,6 @@ int     ft_ten_queens_puzzle(void);
 
 int     main(void)
 {
-    //ft_check("023///////", 2);
-    //ft_ten_queens_puzzle();
     printf(" <- last line\n\n\n%d possibilities.", ft_ten_queens_puzzle());
     return (0);
 }
